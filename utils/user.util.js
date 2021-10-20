6f0487a754cb027cfadd49002d6ebc1f0f4597eb
ihor-vip <vip20140902@gmail.com>
2021-10-20 18:27:17 +0300

    update normaliser

--- utils/user.util.js
+++ utils/user.util.js
@@ -1,13 +1,13 @@
 module.exports = {
     userNormalizer: (userToNormalize) => {
-        const fileldsToRemove = [
+        const fieldsToRemove = [
             'password',
             '__v'
         ];
 
         userToNormalize = userToNormalize.toObject();
 
-        fileldsToRemove.forEach((field) => {
+        fieldsToRemove.forEach((field) => {
             delete userToNormalize[field];
         });
 
