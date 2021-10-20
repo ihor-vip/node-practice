ce7adaf97dd29e7dba2cc5fc84d1a4a2e41331d9
ihor-vip <vip20140902@gmail.com>
2021-10-20 18:31:19 +0300

    db

--- services/db.service.js
+++ services/db.service.js
@@ -1,6 +1,10 @@
 module.exports = {
     createItem: (schema, newItemData) => schema.create(newItemData),
 
+    deleteItemById: (schema, itemId) => schema.deleteOne({_id: itemId}),
+
+    deleteItem: (schema, filter) => schema.deleteOne(filter),
+
     findItemsByQuery: (schema, query) => schema.find(query),
 
     findItemById: (schema, itemId) => schema.findById(itemId),
@@ -8,10 +12,6 @@ module.exports = {
     findItem: (schema, filter) => schema.findOne(filter),
 
     findItemAndJoin: (schema, filter, tableToJoin) => schema.findOne(filter).populate(tableToJoin),
-
-    deleteItemById: (schema, itemId) => schema.deleteOne({_id: itemId}),
-
-    deleteItem: (schema, filter) => schema.deleteOne(filter),
 
     updateItemById: (schema, itemId, newItemData) => schema.updateOne({_id: itemId}, newItemData)
 };
