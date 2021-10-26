931d813ff36dbff633f378e45860667a5cb8866c
ihor-vip <vip20140902@gmail.com>
2021-10-26 09:50:37 +0300

    cron, helmet

--- cron/old-token-remove.js
+++ cron/old-token-remove.js
@@ -3,12 +3,12 @@ const utc = require('dayjs/plugin/utc');
 
 dayJS.extend(utc);
 
-const {O_Auth} = require('../dataBase');
+const {TokenAuth} = require('../dataBase');
 
 module.exports = async () => {
     const previousMonth = dayJS.utc().subtract(1, 'month');
 
-    const deleteInfo = await O_Auth.deleteMany({
+    const deleteInfo = await TokenAuth.deleteMany({
         createdAt: {$lt: previousMonth}
     });
 
